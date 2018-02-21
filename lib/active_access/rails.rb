84444a3a83254e391b95ab8b270528315b34d82b
George Karaszi <georgekaraszi@gmail.com>
2018-02-21 15:38:40 -0500

    Use config to determine if middleware should be enabled

--- lib/active_access/rails.rb
+++ lib/active_access/rails.rb
@@ -3,7 +3,7 @@
 module ActiveAccess
   class Railtie < Rails::Railtie
     initializer "active_access.configure_rails_initialization" do
-      if use_better_errors?
+      if use_active_access?
         insert_middleware
         ActiveAccess.logger           = Rails.logger
         ActiveAccess.application_root = Rails.root.to_s
@@ -18,8 +18,8 @@ module ActiveAccess
       end
     end
 
-    def use_better_errors?
-      !Rails.env.production? && app.config.consider_all_requests_local
+    def use_active_access?
+      ActiveAccess.config.enabled?
     end
 
     def app
